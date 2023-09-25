f8b5435127a4d8f1e4ca5971338d2e71e85aeee5
Martin <martin_lenart@icloud.com>
2023-09-25 13:26:25 +0200

    initial commit

---
+++ 01_class_basics/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01_class_basics;
+
+class Program
+{
+    public class Stock
+    {
+        public decimal Price { get; set; } = 0;
+        public int Amount { get; set; } = 0;
+
+        public string Name { get; } = null;
+
+        public decimal TotValue()
+        {
+            var _amount = Price * Amount;
+            return _amount;
+        }
+        public string WhoAmI()
+        {
+            var _s = $"My {Name} portfolio is worth {TotValue():N2} Sek";
+            return _s;
+        }
+
+        public Stock()
+        {
+            Name = "Volvo";
+            Price = 35.30M;
+            Amount = 1000;
+        }
+        public Stock(string _name, decimal _price, int _amount)
+        {
+            Name = _name;
+            Price = _price;
+            Amount = _amount;
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        var stock1 = new Stock();
+        Console.WriteLine(stock1.WhoAmI());
+
+        var stock2 = new Stock("SAS", 2.35M, 100);
+        Console.WriteLine(stock2.WhoAmI());
+    }
+}
+
