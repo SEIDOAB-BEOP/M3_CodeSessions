be5ed53e0691e06461b9f11c819e446667939306
Martin <martin_lenart@icloud.com>
2023-10-20 11:17:57 +0200

    update

--- 06_embedded_list/Program.cs
+++ 06_embedded_list/Program.cs
@@ -10,20 +10,19 @@ class Program
         Console.WriteLine("06_embedded_list");
 
         var rnd = new csSeedGenerator();
-/*
+
         var p1 = new csPearl();
         Console.WriteLine(p1);
 
         var p2 = new csPearl(rnd);
         Console.WriteLine(p2);
-*/
+
         var pr = new rePearl(rnd);
         Console.WriteLine(pr);
 
         var pr_copy = pr with { Color = enPearlColor.Pink };
         Console.WriteLine(pr_copy);
  
-/*
         var n = new csNecklace(3, "African with Pearls as a class");
         Console.WriteLine(n);
 
@@ -32,7 +31,6 @@ class Program
        
         var n1 = new csNecklace1(15, "European with Pearls as a record");
         Console.WriteLine(n1);
-*/
     }
 }
 
